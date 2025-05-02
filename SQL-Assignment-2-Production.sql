b8d825fe99a8aab162d2ed2429e76fabdff4920f
r101409043 <34722067+r101409043@users.noreply.github.com>
2025-05-02 02:22:31 -0400

    Update SQL-Assignment-2-Production.sql

--- SQL-Assignment-2-Production.sql
+++ SQL-Assignment-2-Production.sql
@@ -1,11 +1,13 @@
 -- How many products can you find in the Production.Product table?
 SELECT COUNT(*) AS CountedProducts
 FROM Production.Product p;
+
 -- Write a query that retrieves the number of products in the Production.Product table that are included in a subcategory. 
 -- The rows that have NULL in column ProductSubcategoryID are considered to not be a part of any subcategory.
 SELECT COUNT(*) AS CountedProducts
 FROM Production.Product p
 WHERE p.ProductSubcategoryID IS NOT NULL;
+
 -- How many Products reside in each SubCategory? Write a query to display the results with the following titles.
 -- ProductSubcategoryID CountedProducts
 -- ------------------ ---------------
@@ -13,13 +15,16 @@ SELECT p.ProductSubcategoryID, COUNT(*) AS CountedProducts
 FROM Production.Product p
 WHERE p.ProductSubcategoryID IS NOT NULL
 GROUP BY ProductSubcategoryID;
+
 -- How many products that do not have a product subcategory.
 SELECT COUNT(*) AS CountedProducts
 FROM Production.Product p
 WHERE p.ProductSubcategoryID IS NULL;
+
 -- Write a query to list the sum of products quantity in the Production.ProductInventory table.
 SELECT SUM(p.quantity) AS TheSum
 FROM Production.ProductInventory p;
+
 -- Write a query to list the sum of products in the Production.ProductInventory table and 
 -- LocationID set to 40 and limit the result to include just summarized quantities less than 100.
 -- ProductID    TheSum
@@ -29,6 +34,7 @@ FROM Production.ProductInventory p
 WHERE LocationID = 40
 GROUP BY ProductID
 HAVING SUM(p.quantity) < 100;
+
 -- Write a query to list the sum of products with the shelf information in the Production.ProductInventory table and 
 -- LocationID set to 40 and limit the result to include just summarized quantities less than 100
 -- Shelf      ProductID    TheSum
@@ -38,17 +44,20 @@ FROM Production.ProductInventory p
 WHERE LocationID = 40
 GROUP BY p.Shelf, p.ProductID
 HAVING SUM(p.quantity) < 100;
+
 -- Write the query to list the average quantity for products where 
 -- column LocationID has the value of 10 from the table Production.ProductInventory table.
 SELECT AVG(p.quantity) AS TheAvg
 FROM Production.ProductInventory p
 WHERE LocationID = 10;
+
 -- Write query  to see the average quantity  of  products by shelf  from the table Production.ProductInventory
 -- ProductID   Shelf      TheAvg
 -- --------- ---------- -----------
 SELECT p.ProductID, p.Shelf, AVG(p.quantity) AS TheAvg
 FROM Production.ProductInventory p
 GROUP BY p.ProductID, p.Shelf;
+
 -- Write query  to see the average quantity  of  products by shelf excluding rows that 
 -- has the value of N/A in the column Shelf from the table Production.ProductInventory
 -- ProductID   Shelf      TheAvg
@@ -57,6 +66,7 @@ SELECT p.ProductID, p.Shelf, AVG(p.quantity) AS TheAvg
 FROM Production.ProductInventory p
 WHERE Shelf != 'N/A'
 GROUP BY p.ProductID, p.Shelf;
+
 -- List the members (rows) and average list price in the Production.Product table. 
 -- This should be grouped independently over the Color and the Class column. Exclude the rows where Color or Class are null.
 -- Color       Class      TheCount     AvgPrice
